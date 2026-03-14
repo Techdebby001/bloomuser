74b6cf328d5c0f70fe79b1b4da8c9991faa3a70f
Techdebby001 <amagold08@gmail.com>
2026-03-14 22:49:37 +0100

    My first push to server

---
+++ userapps/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+GENDER =(
+    ('M','Male'),
+    ('F','Female'),
+)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=225)
+    username = models.CharField(max_length=50, primary_key=True)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=6,choices=GENDER)
+    phone = models.CharField(max_length=15)
+    profile_pix = models.ImageField(upload_to="profile",default="me.jpg",null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.fullname
+
